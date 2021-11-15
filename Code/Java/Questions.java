4b3ae2dd687f4757f763b2514cebfd368330cbfa
MARALucas <lucas.mara@efrei.net>
2021-11-15 16:43:06 +0100

    ajout de methode dans Questions

--- Code/Java/Questions.java
+++ Code/Java/Questions.java
@@ -2,5 +2,26 @@ package pooprojet;
 
 class Questions
 {
-  
+  private ArrayList<Question> Quest;
+
+  public Question() 
+  {
+      Quest = ArrayList<>();
+  }
+
+  public void add(Question q) 
+  {
+      Quest.add(q);
+  }
+
+  public void remove(int i)
+  {
+      Quest.remove(i);
+  }
+
+  @Override
+  public String toString() 
+  {
+      return 
+  }
 }
