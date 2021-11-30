f36fcbdd77c99f0d52ded7617573551561cf435a
VincentNOURY <vincent.noury@efrei.net>
2021-11-30 16:31:55 +0100

    Fix #18

--- Code/Java/Themes.java
+++ Code/Java/Themes.java
@@ -29,6 +29,20 @@ class Themes
     throw new IllegalArgumentException("Le theme spécifié n'existe pas");
   }
 
+  public String[] getRandomThemes(){
+    ArrayList<String> themes = new ArrayList<>();
+    ArrayList<Integer> alreadySelected = new ArrayList<>();
+    for (int i = 0; i < 5; i++){
+      int index = (int)(Math.random() * (themeList.size() + 1));
+      if (!alreadySelected.contains(index)){
+        alreadySelected.add(index);
+        themes.add(themeList.get(index));
+      }
+    }
+    String[] ret = {themes.get(0), themes.get(1), themes.get(2), themes.get(3), themes.get(4)};
+    return ret;
+  }
+
   @Override
   public String toString()
   {
